f639a03ad7b8207492c311b7f20d9156c5593457
GabiOC <oconnor.gabriele@gmail.com>
2015-10-12 11:45:36 -0400

    all done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  num_array = [*1..limit-1]
+  num_array.map do |num|
+    if num % 5 == 0 || num % 3 == 0
+      num
+    end
+  end.compact
+end
+
+def sum_multiples(limit)
+  num_array = collect_multiples(limit)
+  num_array.inject{|sum, num| sum + num}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,21 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(num)
+    @num = num
+    raise ArgumentError unless @num != nil
+  end
+
+  def collect_multiples
+    @num_array = [*1..@num-1]
+    @num_array.map do |num|
+      if num % 5 == 0 || num % 3 == 0
+        num
+      end
+    end.compact
+  end
+
+  def sum_multiples
+    collect_multiples.inject{|sum, num| sum + num}
+  end
+end
